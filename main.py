e84265205125ccc7823bb0fa372f1be04b30f70b
Adam Hing <usl301@usask.ca>
2025-01-25 22:25:43 -0700

    Add new functions and integrate Ollama chat functionality in script

---
+++ main.py
@@ -0,0 +1,42 @@
+import ollama
+import os
+
+import subprocess
+import re
+
+
+def get_git_diff():
+    try:
+        return subprocess.check_output(['git', 'diff', '--cached', '--diff-algorithm=minimal']).decode('utf-8')
+    except subprocess.CalledProcessError:
+        return "No staged changes found."
+
+
+print(get_git_diff())
+print("============================================================")
+
+def chat():
+    diff = get_git_diff()
+    response = ollama.chat(model='deepseek-r1:8b',messages=[
+        {
+            'role': 'system',
+            'content': "Generate a git commit message. Respond with ONLY the message. 15 words maximum."
+        },
+        {
+            'role': 'user',
+            'content': f"Generate a commit message for this diff:\n\n{diff}"
+        }
+    ])
+
+    return response['message']['content']
+#print(chat())
+
+
+
+current_file = open(os.path.basename(__file__),'r')
+current_file = current_file.read()
+print(current_file)
+
+response=chat()
+response = re.sub(r"<think>.*?</think>", "", response, flags=re.DOTALL).strip()
+print(response)
