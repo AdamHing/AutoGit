40f3f7b6b924eb8b5d0c256814f06116aed3f5e7
Adam Hing <usl301@usask.ca>
2025-01-26 18:05:59 -0700

    Improve error handling for git diff and enhance message generation.

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import ollama
 import subprocess
 import re
 
-
 def get_git_diff():
     try:
         return subprocess.check_output(['git', 'diff', '--cached', '--diff-algorithm=minimal']).decode('utf-8')
     except subprocess.CalledProcessError:
         return "No staged changes found."
-
 
 print(get_git_diff())
 print("============================================================")
@@ -18,7 +16,7 @@ def generate_commit_msg():
     response = ollama.chat(model='deepseek-r1:8b',messages=[
         {
             'role': 'system',
-            'content': "Generate a git commit message. Respond with ONLY the message. 15 words maximum."
+            'content': "Generate a git commit message based on the change log of the file. Respond with ONLY the message. 20 words maximum."
         },
         {
             'role': 'user',
@@ -32,3 +30,14 @@ def generate_commit_msg():
 response=generate_commit_msg()
 response = re.sub(r"<think>.*?</think>", "", response, flags=re.DOTALL).strip()
 print(response)
+
+
+
+#generate git log
+
+# git push
+# git add
+
+# auto adding 
+
+# 
