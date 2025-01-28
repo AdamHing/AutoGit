2714138bc9d0a4fc5a8edeb5191f249b72b4a64d
Adam Hing <usl301@usask.ca>
2025-01-28 13:38:40 -0700

    Add keep_alive parameter and improve ollama.chat response extraction.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def generate_commit_msg():
     diff = get_git_diff()
     
     if diff != "No staged changes found.":
-        response = ollama.chat(model='deepseek-r1:8b',messages=[
+        response = ollama.chat(model='deepseek-r1:8b',keep_alive=60,messages=[
              {
             'role': 'system',
             'content': 'You are a helpful assistant tasked with creating concise and meaningful Git commit messages. Your responses must fit within 15 words and describe the exact nature and purpose of the code changes.'
@@ -24,10 +24,14 @@ def generate_commit_msg():
             'content': f"Analyze the following diff and generate a descriptive Git commit message. Be specific about the change and its impact:\n\n{diff}"
         }
         ])
+        
         return re.sub(r"<think>.*?</think>", "", response['message']['content'], flags=re.DOTALL).strip()
-    
+
     else:
         return "updated nothing."
+    
+    
+    
     
 
 repo = git.Repo() 
